2a925636e8bd0a6cee36adc35420d1649c373979
TeeMee123 <123teemee@gmail.com>
2020-07-26 14:30:48 +0100

    Initial commit - Default generated LibGDX project
    Can test on desktop, and also html with superDev gradle task.

---
+++ core/src/teemee/game/gt/world/underground/Chunk.java
@@ -0,0 +1,14 @@
+package teemee.game.gt.world.underground;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Chunk {
+
+    //Data on what is mined
+    private byte[] quarryLevelMap;
+
+    //ArrayList<Point>
+
+
+}
---
+++ desktop/src/teemee/game/gt/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package teemee.game.gt.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import teemee.game.gt.GTGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GTGame(), config);
+	}
+}
